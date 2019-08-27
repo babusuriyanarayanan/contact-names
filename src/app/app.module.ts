d42d9f1eb3281129ed8832617d36123d600756e4
babusuriyanarayanan <babusuriyanarayanan@gmail.com>
2019-08-27 11:15:24 +0530

    feat(added bootstrap): adding bootstrap to support responsive layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { ContentComponent } from './modules/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
